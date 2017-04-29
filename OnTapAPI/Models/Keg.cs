8bd18877c884867da3cfffe4c1f6a9a73bb4cf1f
Terje Tjervaag <terje@tjervaag.com>
2017-04-29 19:42:19 +0200

    Initial commit, moved from ontap repo

---
+++ OnTapAPI/Models/Keg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OnTapAPI.Models
+{
+    public class Keg
+    {
+        public int kegId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public Keg()
+        {
+        }
+    }
+}
---
+++ OnTapAPI/Models/Tap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OnTapAPI.Models
+{
+    public class Tap
+    {
+        public int tapId { get; set; }
+        public int kegId { get; set; }
+
+        public Tap()
+        {
+            
+        }
+    }
+}
